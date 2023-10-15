fd2db256c429f6525d0ed2994b4d28b48fc74070
Dev.Jay <109913616+jjaehha@users.noreply.github.com>
2023-10-16 00:15:30 +0900

    [level unrated] Title: 양꼬치, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120830. 양꼬치/양꼬치.js
@@ -0,0 +1,10 @@
+function solution(n, k) {
+    var answer = 0;
+    if (n >= 10) {
+        k -= Math.floor(n/10)
+        answer = (n*12000) + (k*2000)
+    } else {
+        answer = (n*12000) + (k*2000)
+    }
+    return answer;
+}
