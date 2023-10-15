8fbb0d388a07aa8a18c33d563bee4a4985e53334
Dev.Jay <109913616+jjaehha@users.noreply.github.com>
2023-10-15 23:48:22 +0900

    [level unrated] Title: 각도기, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120829. 각도기/각도기.js
@@ -0,0 +1,15 @@
+function solution(angle) {
+    var answer = 0;
+    if (0 < angle && angle < 90) {
+        answer = 1;
+    } else if (angle === 90) {
+        answer = 2;
+    } else if (90 < angle && angle < 180) {
+        answer = 3;
+    } else if (angle === 180) {
+        answer = 4;
+    } else {
+        consol.log("입력값 오류")
+    }
+    return answer;
+}
